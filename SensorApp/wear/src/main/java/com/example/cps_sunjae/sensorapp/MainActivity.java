29303ac6e9ab1e66a1ab07b2746390d2e7a25ca0
sangjin <hhclover@kaist.ac.kr>
2018-07-17 12:30:50 +0900

    local store

--- SensorApp/wear/src/main/java/com/example/cps_sunjae/sensorapp/MainActivity.java
+++ SensorApp/wear/src/main/java/com/example/cps_sunjae/sensorapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.cps_sunjae.sensorapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -21,102 +28,147 @@ import com.google.android.gms.wearable.Wearable;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 
-public class MainActivity extends WearableActivity implements SensorEventListener, GoogleApiClient.ConnectionCallbacks,
-    GoogleApiClient.OnConnectionFailedListener {
 
-    private TextView mAccelX;
-    private TextView mAccelY;
-    private TextView mAccelZ;
+public class MainActivity extends WearableActivity implements SensorEventListener {
+
+    // CSV files
+    private BufferedWriter bw = null;
+
+    private String filename = "sensor_data.txt";
+    private File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+    private File file;
+    FileOutputStream outputStream;
+
+    // Arrays to hold sensor data
+    private ArrayList<Float> accelX = new ArrayList<>();
+    private ArrayList<Float> accelY = new ArrayList<>();
+    private ArrayList<Float> accelZ = new ArrayList<>();
+    private ArrayList<Float> gyroX = new ArrayList<>();
+    private ArrayList<Float> gyroY = new ArrayList<>();
+    private ArrayList<Float> gyroZ = new ArrayList<>();
+
+    // Sensors
     private SensorManager mSensorManager;
     private Sensor mAccel;
     private Sensor mGyro;
-    private GoogleApiClient mGoogleApiClient;
 
+    // Sensor variables
+    private boolean start = false;
+    private String recording = "Recording...";
+    private String stop = "Stop";
+
+    // Textview
+    TextView currentLabel;
+
+    //Veryify storage permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Called when the activity is first created
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAccelX = (TextView) findViewById(R.id.accelX);
-        mAccelY = (TextView) findViewById(R.id.accelY);
-        mAccelZ = (TextView) findViewById(R.id.accelZ);
-
-        // Enables Always-on
+        //Enables Always-on
         setAmbientEnabled();
 
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        file = new File(path, filename);
+
+        // Get an instance of the SensorManager
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        // Create the listeners for each sensor type
         mAccel = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mGyro = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Wearable.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-        mGoogleApiClient.connect();
-    }
+        // Set Labels
+        currentLabel = (TextView) findViewById(R.id.currentActivity);
 
-    @Override
-    public void onConnected(Bundle bundle) {
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
-    }
-
-    public void sendSensorData(float accelX, float accelY, float accelZ) {
-        PutDataMapRequest putDataMapRequest = PutDataMapRequest.create("/sensorData");
-
-//        putDataMapRequest.getDataMap().putLong("timestamp", timestamp);
-        putDataMapRequest.getDataMap().putFloat("accelX", accelX);
-        putDataMapRequest.getDataMap().putFloat("accelY", accelY);
-        putDataMapRequest.getDataMap().putFloat("accelZ", accelZ);
-
-        PutDataRequest request = putDataMapRequest.asPutDataRequest();
-        Wearable.DataApi.putDataItem(mGoogleApiClient, request)
-                .setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
-                    @Override
-                    public void onResult(@NonNull DataApi.DataItemResult dataItemResult) {
-                        if (!dataItemResult.getStatus().isSuccess()) {
-                            Log.d("sunjae", "send Successful");
-                        } else {
-
-                        }
+        // Set Buttons
+        findViewById(R.id.btn_start).setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        start = true;
+                        currentLabel.setText(recording);
                     }
-                });
+                }
+        );
+        findViewById(R.id.btn_stop).setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        start = false;
+                        currentLabel.setText(stop);
+                        sendSensorData();
+                    }
+                }
+        );
+    }
+
+    /**
+     * Called each time the sensor changes. Fills buffers to be written to the CSV.
+     *
+     * @param event The event corresponding to a sensor
+     */
+    public void onSensorChanged(SensorEvent event) {
+        if (start) {
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                getAcceleration(event);
+            } else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+                getGyroscope(event);
+            }
+        }
+    }
+
+    private void getAcceleration(SensorEvent event) {
+        float accX = event.values[0];
+        float accY = event.values[1];
+        float accZ = event.values[2];
+
+        // Record the values
+        if (start) {
+            accelX.add(accX);
+            accelY.add(accY);
+            accelZ.add(accZ);
+        }
+    }
+
+    private void getGyroscope(SensorEvent event) {
+        float gyX = event.values[0];
+        float gyY = event.values[1];
+        float gyZ = event.values[2];
+
+        // Record the values
+        if (start) {
+            gyroX.add(gyX);
+            gyroY.add(gyY);
+            gyroZ.add(gyZ);
+        }
     }
 
     @Override
-    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    @Override
-    public final void onSensorChanged(SensorEvent event) {
-        float accelX = event.values[0];
-        float accelY = event.values[1];
-        float accelZ = event.values[2];
-
-
-        mAccelX.setText(String.valueOf(accelX) + "\n");
-        mAccelY.setText(String.valueOf(accelY) + "\n");
-        mAccelZ.setText(String.valueOf(accelZ));
-        sendSensorData(accelX, accelY, accelZ);
-
+    public void onAccuracyChanged(Sensor sensor, int i){
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this,mAccel,SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mAccel, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mGyro, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
@@ -124,4 +176,30 @@ public class MainActivity extends WearableActivity implements SensorEventListene
         super.onPause();
         mSensorManager.unregisterListener(this);
     }
+
+
+    /**
+     * Make CSV file and send to phone
+     */
+    public void sendSensorData(){
+        try{
+            outputStream = new FileOutputStream(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //accel
+        for (int i = 0; i < accelX.size(); i++) {
+            String accel = Float.toString(accelX.get(i)) + ", " +
+                    Float.toString(accelY.get(i)) + ", " +
+                    Float.toString(accelZ.get(i)) + "\n";
+            System.out.println(accel);
+            try {
+                outputStream.write(accel.getBytes());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
