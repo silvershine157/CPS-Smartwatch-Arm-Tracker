be4568b4e8eb28369a320b996ca47b13cd88f8e0
Ahyun Kim <nonnos@kaist.ac.kr>
2018-08-01 17:51:23 +0900

    Button on Notification & Two Wearable availabe

--- SensorApp/mobile/src/main/java/com/example/cps_sunjae/sensorapp/MainActivity.java
+++ SensorApp/mobile/src/main/java/com/example/cps_sunjae/sensorapp/MainActivity.java
@@ -5,59 +5,26 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Color;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.provider.Settings;
-import android.support.annotation.WorkerThread;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.os.Environment;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
-import com.google.android.gms.wearable.Asset;
-import com.google.android.gms.wearable.DataClient;
-import com.google.android.gms.wearable.DataEvent;
-import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.DataItem;
-import com.google.android.gms.wearable.DataMap;
-import com.google.android.gms.wearable.DataMapItem;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.Wearable;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Collection;
-import java.util.Date;
-import java.util.Calendar;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "SensorApp";
-    private static final String START_SENSING_PATH = "/start-sensing";
-    private static final String STOP_SENSING_PATH = "/stop-sensing";
+    private static final String START_MESSAGE = "start.message";
+    private static final String STOP_MESSAGE = "stop.message";
 
     static TextView currentStatus;
-    Button btn, btn_dwn;
+    Button btn_start, btn_stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,25 +36,17 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), SensorService.class);
         startService(intent);
 
-        currentStatus = (TextView) findViewById(R.id.status);
-        btn = findViewById(R.id.btn);
-        btn_dwn = findViewById(R.id.btn_downloads);
-        btn.setOnClickListener(new Button.OnClickListener(){
+        currentStatus = findViewById(R.id.status);
+        btn_start = findViewById(R.id.btn_start);
+        btn_stop = findViewById(R.id.btn_stop);
+        btn_start.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
-                btnClick();
+                btnStart();
             }
         });
-        btn_dwn.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                String pkg = "com.android.documentsui";
-                if (checkPackage(pkg)) {
-                    Intent intent = getPackageManager().getLaunchIntentForPackage(pkg);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(intent);
-                } else {
-                    Log.d(TAG, "no package");
-                    Toast.makeText(getApplicationContext(), "Need " + pkg, Toast.LENGTH_LONG).show();
-                }
+        btn_stop.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                btnStop();
             }
         });
         checkPermission();
@@ -97,83 +56,19 @@ public class MainActivity extends AppCompatActivity {
         currentStatus.setText(str);
     }
 
-    public static void setColor(int r, int g, int b) {
+    public static void setColor(final int r, final int g, final int b) {
         currentStatus.setBackgroundColor(Color.rgb(r, g, b));
     }
 
-    public boolean checkPackage(String pkg) {
-        boolean isExist = false;
-        PackageManager pkgMgr = getPackageManager();
-        List<ResolveInfo> mApps;
-        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        mApps = pkgMgr.queryIntentActivities(mainIntent, 0);
-
-        try {
-            for (int i = 0; i < mApps.size(); i++) {
-                if (mApps.get(i).activityInfo.packageName.equals(pkg)) {
-                    isExist = true;
-                    break;
-                }
-            }
-        } catch (Exception e) {
-            Log.d(TAG, e.toString());
-        }
-        return isExist;
+    private void btnStart() {
+        currentStatus.setText("Recording");
+        Intent intent = new Intent(START_MESSAGE);
+        sendBroadcast(intent);
     }
 
-    private void btnClick(){
-        new SendTask().execute();
-    }
-
-    @WorkerThread
-    private Collection<String> getNodes() {
-        HashSet<String> results = new HashSet<>();
-
-        Task<List<Node>> nodeListTask =
-                Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
-        try {
-            List<Node> nodes = Tasks.await(nodeListTask);
-            for (Node node : nodes) {
-                results.add(node.getId());
-            }
-        } catch (Exception e) {
-            Log.d(TAG, "E: " + e);
-        }
-
-        return results;
-    }
-
-    private class SendTask extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void... args) {
-            Collection<String> nodes = getNodes();
-            for (String node : nodes) {
-                sendMessage(node);
-            }
-            return null;
-        }
-    }
-
-    public void sendMessage(String node) {
-        if (btn.getText().equals("start")) {
-            Wearable.getMessageClient(this).sendMessage(node, START_SENSING_PATH, new byte[0]);
-            this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    btn.setText("stop");
-                    currentStatus.setText("Recording");
-                }
-            });
-        } else {
-            Wearable.getMessageClient(this).sendMessage(node, STOP_SENSING_PATH, new byte[0]);
-            this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    btn.setText("start");
-                }
-            });
-        }
+    private void btnStop() {
+        Intent intent = new Intent(STOP_MESSAGE);
+        sendBroadcast(intent);
     }
 
     public void checkPermission() {
--- SensorApp/mobile/src/main/java/com/example/cps_sunjae/sensorapp/SensorService.java
+++ SensorApp/mobile/src/main/java/com/example/cps_sunjae/sensorapp/SensorService.java
@@ -3,20 +3,24 @@ package com.example.cps_sunjae.sensorapp;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Environment;
 import android.os.IBinder;
+import android.support.annotation.WorkerThread;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
-import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.android.gms.wearable.Asset;
 import com.google.android.gms.wearable.DataClient;
 import com.google.android.gms.wearable.DataEvent;
@@ -24,8 +28,8 @@ import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.DataItem;
 import com.google.android.gms.wearable.DataMap;
 import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.Wearable;
-import com.google.android.gms.wearable.WearableListenerService;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -35,7 +39,10 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 
 public class SensorService extends Service implements DataClient.OnDataChangedListener {
@@ -43,15 +50,19 @@ public class SensorService extends Service implements DataClient.OnDataChangedLi
     private static final String TAG = "SensorAppService";
 
     private static final String SENSOR_ACCEL = "sensor.accel";
-    private static final String SENSOR_GYRO = "sensor.gyro";
-    private static final String SENSOR_MAG = "sensor.mag";
     private static final String SENSOR_lACCEL = "sensor.laccel";
     private static final String SENSOR_GRAV = "sensor.grav";
     private static final String SENSOR_ROTVEC = "sensor.rotvec";
-    private static final String SENSOR_ORIENT = "sensor.orient";
-    private static final String SENSOR_GROTV = "sensor.grotv";
+
+    private static final String START_SENSING_PATH = "/start-sensing";
+    private static final String STOP_SENSING_PATH = "/stop-sensing";
+
+    private static final String START_MESSAGE = "start.message";
+    private static final String STOP_MESSAGE = "stop.message";
 
     private static String CHANNEL_ID = "ChannelID";
+
+    private BroadcastReceiver mReceiver;
 
     Date d;
     SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
@@ -64,21 +75,40 @@ public class SensorService extends Service implements DataClient.OnDataChangedLi
         super.onCreate();
         Log.d(TAG, "onCreate()");
         Wearable.getDataClient(this).addListener(this);
-    }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(START_MESSAGE);
+        filter.addAction(STOP_MESSAGE);
+        mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(START_MESSAGE)) {
+                    new SendStartTask().execute();
+                } else if (intent.getAction().equals(STOP_MESSAGE)) {
+                    new SendStopTask().execute();
+                }
+            }
+        };
+        this.registerReceiver(mReceiver, filter);
+
         createNotificationChannel();
+        Intent startIntent = new Intent();
+        Intent stopIntent = new Intent();
+        startIntent.setAction(START_MESSAGE);
+        stopIntent.setAction(STOP_MESSAGE);
+        PendingIntent startPendingIntent = PendingIntent.getBroadcast(this, 0, startIntent, 0);
+        PendingIntent stopPendingIntent = PendingIntent.getBroadcast(this, 0, stopIntent, 0);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
                 .setSmallIcon(R.drawable.noticon)
                 .setContentTitle("In Background service")
-                .setDefaults(Notification.DEFAULT_VIBRATE);
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .addAction(R.drawable.noticon, "start", startPendingIntent)
+                .addAction(R.drawable.noticon, "stop", stopPendingIntent);
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
             builder.setPriority(NotificationCompat.PRIORITY_MAX);
         }
         Notification notification = builder.build();
         startForeground(1, notification);
-        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -107,6 +137,14 @@ public class SensorService extends Service implements DataClient.OnDataChangedLi
         }
     }
 
+    private class WriteTask extends AsyncTask<DataMap, Void, Void> {
+        @Override
+        protected Void doInBackground(DataMap... dataMaps) {
+            write(dataMaps[0]);
+            return null;
+        }
+    }
+
     @Override
     public void onDataChanged(DataEventBuffer dataEvents) {
         Log.d("testdrive", "data changed");
@@ -119,37 +157,44 @@ public class SensorService extends Service implements DataClient.OnDataChangedLi
                 DataItem item = event.getDataItem();
                 if (item.getUri().getPath().compareTo("/sensor") == 0) {
                     Log.d("testdrive", "data received");
+                    MainActivity.makeText("data received");
                     DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
-                    try {
-                        loadFromAsset("_accel.txt", dataMap.getAsset(SENSOR_ACCEL));
-                        loadFromAsset("_grav.txt", dataMap.getAsset(SENSOR_GRAV));
-                        loadFromAsset("_lAccel.txt", dataMap.getAsset(SENSOR_lACCEL));
-                        loadFromAsset("_rotVector.txt", dataMap.getAsset(SENSOR_ROTVEC));
-                        Log.d("testdrive", "data written");
-                        MainActivity.makeText("data written");
-                        Random rand = new Random();
-                        int r = rand.nextInt(256);
-                        int g = rand.nextInt(256);
-                        int b = rand.nextInt(256);
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
-                                .setSmallIcon(R.drawable.noticon)
-                                .setContentTitle("Data Written")
-                                .setDefaults(Notification.DEFAULT_VIBRATE)
-                                .setColor(Color.rgb(r, g, b));
-                        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-                            builder.setPriority(NotificationCompat.PRIORITY_MAX);
-                        }
-                        Notification notification = builder.build();
-                        NotificationManager nm = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-                        nm.notify(2, notification);
-                        MainActivity.setColor(r, g, b);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
+                    //new WriteTask().execute(dataMap);
+                    write(dataMap);
                 }
             }
         }
+    }
+
+    private void write(DataMap dataMap){
+
+        try {
+            loadFromAsset("_accel.txt", dataMap.getAsset(SENSOR_ACCEL));
+            loadFromAsset("_grav.txt", dataMap.getAsset(SENSOR_GRAV));
+            loadFromAsset("_lAccel.txt", dataMap.getAsset(SENSOR_lACCEL));
+            loadFromAsset("_rotVector.txt", dataMap.getAsset(SENSOR_ROTVEC));
+            Log.d("testdrive", "data written");
+            MainActivity.makeText("data written");
+            Random rand = new Random();
+            int r = rand.nextInt(256);
+            int g = rand.nextInt(256);
+            int b = rand.nextInt(256);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                    .setSmallIcon(R.drawable.noticon)
+                    .setContentTitle("Data Written")
+                    .setDefaults(Notification.DEFAULT_VIBRATE)
+                    .setColor(Color.rgb(r, g, b));
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+                builder.setPriority(NotificationCompat.PRIORITY_MAX);
+            }
+            Notification notification = builder.build();
+            NotificationManager nm = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+            nm.notify(2, notification);
+            MainActivity.setColor(r, g, b);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void loadFromAsset(final String fileType, Asset asset) {
@@ -185,6 +230,7 @@ public class SensorService extends Service implements DataClient.OnDataChangedLi
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
     private void writeToFile(String type, String data) {
@@ -194,6 +240,10 @@ public class SensorService extends Service implements DataClient.OnDataChangedLi
         dir.mkdirs();
         String filename = date + "/" + date + type;
         File file = new File(path, filename);
+        if (file.isFile()) {
+            filename = date + "/" + date + "_2" + type;
+            file = new File(path, filename);
+        }
         try {
             FileOutputStream f = new FileOutputStream(file);
             PrintWriter pw = new PrintWriter(f);
@@ -207,5 +257,53 @@ public class SensorService extends Service implements DataClient.OnDataChangedLi
             e.printStackTrace();
         }
 
+    }
+
+    public class SendStartTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... args) {
+            Collection<String> nodes = getNodes();
+            for (String node : nodes) {
+                sendStartMessage(node);
+            }
+            return null;
+        }
+    }
+
+    public class SendStopTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... args) {
+            Collection<String> nodes = getNodes();
+            for (String node : nodes) {
+                sendStopMessage(node);
+            }
+            return null;
+        }
+    }
+
+    public void sendStartMessage(String node) {
+        Wearable.getMessageClient(this).sendMessage(node, START_SENSING_PATH, new byte[0]);
+    }
+
+    public void sendStopMessage(String node) {
+        Wearable.getMessageClient(this).sendMessage(node, STOP_SENSING_PATH, new byte[0]);
+    }
+
+    @WorkerThread
+    private Collection<String> getNodes() {
+        HashSet<String> results = new HashSet<>();
+
+        Task<List<Node>> nodeListTask =
+                Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
+        try {
+            List<Node> nodes = Tasks.await(nodeListTask);
+            for (Node node : nodes) {
+                results.add(node.getId());
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "E: " + e);
+        }
+
+        return results;
     }
 }
