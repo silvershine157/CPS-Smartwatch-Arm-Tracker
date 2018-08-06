42c251afa32b1960b28bab4fa61e0f7216653436
Silvershine157 <silvershine157@gmail.com>
2018-08-06 16:24:08 +0900

    rectify parser

--- kinect_analysis/recorder/parse_kindata.m
+++ kinect_analysis/recorder/parse_kindata.m
@@ -1,24 +1,25 @@
 clear
 rawdat = load('kindata.txt');
-kindata = kinDataTuple;
+kd = kinData;
 for i = 1:size(rawdat, 1)
-    kdt = kinDataTuple;
-    kdt.ts = rawdat(i, 1);
+    kd.ts(i, 1) = rawdat(i, 1);
+    
     if(rawdat(i, 2) > 0)
-        kdt.haveMarker = true;
+        kd.haveMarker(i, 1) = true;
     else
-        kdt.haveMarker = false;
+        kd.haveMarker(i, 1) = false;
     end
-    kdt.mp = rawdat(i, 3:5);
-
+    kd.mp(i, :) = rawdat(i, 3:5);
+    
     if(rawdat(i, 6) > 0)
-        kdt.haveBody = true;
+        kd.haveBody(i, 1) = true;
     else
-        kdt.haveBody = false;
+        kd.haveBody(i, 1) = false;
     end
-    kdt.ls = rawdat(i, 7:9);
-    kdt.le = rawdat(i, 10:12);
-    kdt.lw = rawdat(i, 13:15);
-    kindata(i, 1) = kdt;
-    save('kdat.mat', 'kindata')
+    
+    kd.ls(i, :) = rawdat(i, 7:9);
+    kd.le(i, :) = rawdat(i, 10:12);
+    kd.lw(i, :) = rawdat(i, 13:15);
 end
+save('kdat.mat', 'kd');
+clear i kd
