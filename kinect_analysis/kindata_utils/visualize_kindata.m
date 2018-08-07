45f530d98c6450a7fa00d4bc2eaf4ce456cf7aa6
Silvershine157 <silvershine157@gmail.com>
2018-08-07 12:23:29 +0900

    sync for kinData

--- kinect_analysis/kindata_utils/visualize_kindata.m
+++ kinect_analysis/kindata_utils/visualize_kindata.m
@@ -13,18 +13,20 @@ ePos = ePos(sample_range, :);
 wPos = wPos(sample_range, :);
 sPos = sPos(sample_range, :);
 
+%{
+% Full trajectory
 hold off;
 plot3(mPos(:, 1), mPos(:, 3), mPos(:, 2), '*b');
 hold on;
 plot3(wPos(:, 1), wPos(:, 3), wPos(:, 2), '*r');
 plot3(ePos(:, 1), ePos(:, 3), ePos(:, 2), '*g');
 plot3(sPos(:, 1), sPos(:, 3), sPos(:, 2), '*y');
+%}
 
-%{
+
 % Animation
 skip = 10;
 hold off;
-figure
 hold on;
 for i = 1:skip:size(mPos, 1)
     
@@ -37,4 +39,4 @@ for i = 1:skip:size(mPos, 1)
     drawnow;
     pause(1/rate);
 end
-%}
+
